80d4c5d1c1e84b0d9aa20014afa3e6793fc03f10
Anders Fluur <anders@fluur.net>
2023-11-06 15:31:41 +0000

    Adding support for argument parser

--- name_dot_com_update.py
+++ name_dot_com_update.py
@@ -1,41 +1,11 @@
 #!/usr/bin/env python3
 
+import argparse
 import requests
 import time
 import os
 import sys
 import base64
-
-# Name.com API credentials
-
-
-# Environment variable names
-API_USERNAME_VAR = "API_USERNAME"
-API_TOKEN_VAR = "API_TOKEN"
-
-# Check and assign API_USERNAME
-api_username = os.environ.get("API_USERNAME")
-if not api_username:
-    print(f"Error: Environment variable {API_USERNAME_VAR} is not set.")
-    sys.exit(1) # Exit the script with an error
-
-# Check and assign API_TOKEN
-api_token = os.environ.get(API_TOKEN_VAR)
-if not api_token:
-    print(f"Error: Environment variable {API_TOKEN_VAR} is not set.")
-    sys.exit(1) # Exit the script with an error
-
-# If both environment variables are set and not null, you can proceed
-print("API_USERNAME:", api_username)
-print("API_TOKEN:", api_token)
-
-
-# Name.com domain and record details
-DOMAIN = "fluur.net"
-HOST = "host1"
-
-# External IP check URL (you can use other services if you prefer)
-IP_CHECK_URL = "https://ipinfo.io/ip"
 
 def get_external_ip():
     response = requests.get(IP_CHECK_URL)
@@ -48,62 +18,73 @@ def update_dns(ip):
     # Implement the logic to update the DNS record with the new IP
     # You can make API requests to Name.com to update the DNS record here
     # The API documentation is available at https://www.name.com/api-docs/
-    import requests
 
     # Define the API endpoint and request headers
-    #Test API
-    #api_url = f"https://api.dev.name.com/v4/domains/{DOMAIN}/records"
+    api_url = f"{API_BASE_URL}/domains/{args.domain}/records"
 
-    #Production API
-    api_url = f"https://api.name.com/v4/domains/{DOMAIN}/records"
     # Construct the authentication header
     auth_header = (api_username + ":" + api_token).encode("utf-8")
     auth_header = base64.b64encode(auth_header).decode("utf-8")
 
     # Define the data for adding an "A" record
     data = {
-            "host": HOST,
-            "type": "A",
-            "answer": ip,
-            "ttl": 300
-        }
+        "host": args.host,
+        "type": "A",
+        "answer": ip,
+        "ttl": 300
+    }
 
     # Set the Content-Type header to application/json
     headers = {
         "Authorization": "Basic " + auth_header,
         "Content-Type": "application/json"
     }
+
     # Make the API request with authentication
-    #response = requests.get(api_url, json=data, headers=headers)
-
-    # Create the HTTP request without sending it
-    http_request = requests.Request("POST", api_url, json=data, headers=headers)
-    prepared_request = http_request.prepare()
-
-    # Print the full HTTP request including headers and body
-    print("Full HTTP Request:")
-    print(f"{prepared_request.method} {prepared_request.url}")
-    for key, value in prepared_request.headers.items():
-        print(f"{key}: {value}")
-    print()
-    print(prepared_request.body)
-
-    # To send the request, you can use the following:
-    response = requests.Session().send(prepared_request)
-    #sys.exit(1) # Exit the script with an error
+    response = requests.post(api_url, json=data, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
         # Process the API response data as needed
         print(data)
     else:
-                print(f"Error: Request failed with status code {response.status_code}")
-                print("Response Content:")
-                print(response.text)
-                sys.exit(1) # Exit the script with an error
-          
+        print(f"Error: Request failed with status code {response.status_code}")
+        print("Response Content:")
+        print(response.text)
+        sys.exit(1)  # Exit the script with an error
 
 if __name__ == "__main__":
+    # Define and parse command-line arguments
+    parser = argparse.ArgumentParser(description="Update DNS records with external IP address")
+    parser.add_argument("host", help="Host name")
+    parser.add_argument("domain", help="Domain name")
+    parser.add_argument("-i", "--interval", type=int, default=60, help="Polling interval in seconds")
+    args = parser.parse_args()
+
+    # Name.com API credentials
+    API_USERNAME_VAR = "API_USERNAME"
+    API_TOKEN_VAR = "API_TOKEN"
+
+    # Check and assign API_USERNAME
+    api_username = os.environ.get(API_USERNAME_VAR)
+    if not api_username:
+        print(f"Error: Environment variable {API_USERNAME_VAR} is not set.")
+        sys.exit(1)
+
+    # Check and assign API_TOKEN
+    api_token = os.environ.get(API_TOKEN_VAR)
+    if not api_token:
+        print(f"Error: Environment variable {API_TOKEN_VAR} is not set.")
+        sys.exit(1)
+
+    # Determine the API server based on the api_username
+    if api_username.endswith("-test"):
+        API_BASE_URL = "https://api.dev.name.com/v4"
+    else:
+        API_BASE_URL = "https://api.name.com/v4"
+
+    IP_CHECK_URL = "https://ipinfo.io/ip"
+
     current_ip = ""
     while True:
         new_ip = get_external_ip()
@@ -111,6 +92,4 @@ if __name__ == "__main__":
             print(f"New IP address detected: {new_ip}")
             update_dns(new_ip)
             current_ip = new_ip
-        time.sleep(60)  # Wait for 60 seconds before checking again
-
-
+        time.sleep(args.interval)
