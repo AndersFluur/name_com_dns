66af7e1284499a9a4cb55ac9bb7ae8e23685645b
Anders Fluur <anders@fluur.net>
2023-11-12 09:16:31 +0000

    Preparing for Ubuntu installation
    Modified repo name to namecom_dns
    Environment variables in start script and a file to make installation specific modifications in.
    Log directory is configurable as command line argument
    Installation decsribes Python Venv to be used
    Correction in environment naming for API Key and Username

--- namecom_dns/test_main.py
+++ namecom_dns/test_main.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python3
-
 import unittest
 from unittest.mock import patch, MagicMock, call
-from name_com_dns.name_com import NameCom, get_resource_record
-import name_dot_com_update
+from namecom_dns.namecom import get_resource_record
+import os
+import namecom_update
 
 # Mock environment variables
 def mock_get(var_name, default=None):
@@ -19,18 +18,18 @@ class TestMainLogic(unittest.TestCase):
         #print(f"get for '{var_name}'")
         if var_name == 'REQUESTS_CA_BUNDLE':
             return '/etc/ssl/certs/ca-certificates.crt'  # replace with the actual path to your CA bundle
-        if var_name == 'API_USERNAME':
+        if var_name == 'NAMECOM_APIUSERNAME':
             return 'api_username'
-        if var_name == 'API_TOKEN':
+        if var_name == 'NAMECOM_APITOKEN':
             return 'api_token'
         else:
             return 'sample_value'
         
-    @patch('name_dot_com_update.get_external_ip')
+    @patch('namecom_update.get_external_ip')
     @patch('os.environ.get')
     @patch('argparse.ArgumentParser.parse_args')
-    @patch('name_dot_com_update.NameCom')
-    def test_main_logic_rr_exist_new_IP(self, mock_name_com, mock_parse_args, mock_get, mock_get_external_ip):
+    @patch('namecom_update.NameCom')
+    def test_main_logic_rr_exist_new_IP(self, mock_namecom, mock_parse_args, mock_get, mock_get_external_ip):
         """
          Verify that the main logic works as expected when the host record is found but the IP address is different.
         """
@@ -45,38 +44,40 @@ class TestMainLogic(unittest.TestCase):
         args.domain = 'example.com'
         args.interval = 2
         args.test = True
+        args.log = True
+        args.logdir = os.getcwd()
         mock_parse_args.return_value = args
 
         # Mock NameCom class and its methods
-        mock_name_com_instance = mock_name_com.return_value
-        mock_name_com_instance.read_host_record.return_value = get_resource_record(id = 12345, host='host1', ip='1.2.3.4')
+        mock_namecom_instance = mock_namecom.return_value
+        mock_namecom_instance.read_host_record.return_value = get_resource_record(id = 12345, host='host1', ip='1.2.3.4')
         
-        #mock_name_com_instance.create_record.return_value = 12345
-        mock_name_com_instance.update_record.return_value = 12345
+        #mock_namecom_instance.create_record.return_value = 12345
+        mock_namecom_instance.update_record.return_value = 12345
 
         try:
-            name_dot_com_update.main()
+            namecom_update.main()
         except SystemExit as e:
             self.assertEqual(e.code, 0)  # assert that the exit code is 0
     
         calls = mock_get.call_args_list
-        api_username_calls = [call for call in calls if call[0][0] == 'API_USERNAME']
-        api_token_calls = [call for call in calls if call[0][0] == 'API_TOKEN']
+        api_username_calls = [call for call in calls if call[0][0] == 'NAMECOM_APIUSERNAME']
+        api_token_calls = [call for call in calls if call[0][0] == 'NAMECOM_APITOKEN']
         self.assertEqual(len(api_username_calls), 1)
         self.assertEqual(len(api_token_calls), 1)
 
         # Assert that the methods were called with the correct arguments
-        mock_name_com.assert_called_with('api_username', 'api_token', 'example.com', 'host1')
-        mock_name_com_instance.read_host_record.assert_called_once()
-        #mock_name_com_instance.create_record.assert_called_once_with('1.2.3.4')
-        mock_name_com_instance.update_record.assert_called_once_with(12345, '1.2.3.5')
+        mock_namecom.assert_called_with('api_username', 'api_token', 'example.com', 'host1')
+        mock_namecom_instance.read_host_record.assert_called_once()
+        #mock_namecom_instance.create_record.assert_called_once_with('1.2.3.4')
+        mock_namecom_instance.update_record.assert_called_once_with(12345, '1.2.3.5')
         mock_get_external_ip.assert_called()
 
-    @patch('name_dot_com_update.get_external_ip')
+    @patch('namecom_update.get_external_ip')
     @patch('os.environ.get')
     @patch('argparse.ArgumentParser.parse_args')
-    @patch('name_dot_com_update.NameCom')
-    def test_main_logic_no_rr_same_ip_address(self, mock_name_com, mock_parse_args, mock_get, mock_get_external_ip):
+    @patch('namecom_update.NameCom')
+    def test_main_logic_no_rr_same_ip_address(self, mock_namecom, mock_parse_args, mock_get, mock_get_external_ip):
         """
          Verify that the main logic works as expected when no host record is found, but there is no change of IP address.
         """
@@ -91,38 +92,40 @@ class TestMainLogic(unittest.TestCase):
         args.domain = 'example.com'
         args.interval = 2
         args.test = True
+        args.log = True
+        args.logdir = os.getcwd()
         mock_parse_args.return_value = args
 
         # Mock NameCom class and its methods
-        mock_name_com_instance = mock_name_com.return_value
-        mock_name_com_instance.read_host_record.return_value = None
+        mock_namecom_instance = mock_namecom.return_value
+        mock_namecom_instance.read_host_record.return_value = None
         
-        mock_name_com_instance.create_record.return_value = 12345
-        mock_name_com_instance.update_record.return_value = 12345
+        mock_namecom_instance.create_record.return_value = 12345
+        mock_namecom_instance.update_record.return_value = 12345
 
         try:
-            name_dot_com_update.main()
+            namecom_update.main()
         except SystemExit as e:
             self.assertEqual(e.code, 0)  # assert that the exit code is 0
     
         calls = mock_get.call_args_list
-        api_username_calls = [call for call in calls if call[0][0] == 'API_USERNAME']
-        api_token_calls = [call for call in calls if call[0][0] == 'API_TOKEN']
+        api_username_calls = [call for call in calls if call[0][0] == 'NAMECOM_APIUSERNAME']
+        api_token_calls = [call for call in calls if call[0][0] == 'NAMECOM_APITOKEN']
         self.assertEqual(len(api_username_calls), 1)
         self.assertEqual(len(api_token_calls), 1)
 
         # Assert that the methods were called with the correct arguments
-        mock_name_com.assert_called_with('api_username', 'api_token', 'example.com', 'host1')
-        mock_name_com_instance.read_host_record.assert_called_once()
-        mock_name_com_instance.create_record.assert_called_once_with('1.2.3.5')
-        #mock_name_com_instance.update_record.assert_called_once_with(12345, '1.2.3.5')
+        mock_namecom.assert_called_with('api_username', 'api_token', 'example.com', 'host1')
+        mock_namecom_instance.read_host_record.assert_called_once()
+        mock_namecom_instance.create_record.assert_called_once_with('1.2.3.5')
+        #mock_namecom_instance.update_record.assert_called_once_with(12345, '1.2.3.5')
         mock_get_external_ip.assert_called()
 
-    @patch('name_dot_com_update.get_external_ip', side_effect=['1.2.3.5', '1.2.3.4', '1.2.3.4', '1.2.3.5'])
+    @patch('namecom_update.get_external_ip', side_effect=['1.2.3.5', '1.2.3.4', '1.2.3.4', '1.2.3.5'])
     @patch('os.environ.get')
     @patch('argparse.ArgumentParser.parse_args')
-    @patch('name_dot_com_update.NameCom')
-    def test_main_logic_no_rr_change_ip_address(self, mock_name_com, mock_parse_args, mock_get, mock_get_external_ip):
+    @patch('namecom_update.NameCom')
+    def test_main_logic_no_rr_change_ip_address(self, mock_namecom, mock_parse_args, mock_get, mock_get_external_ip):
         """
          Verify that the main logic works as expected when no host record is found.
          There is a change of IP address from when the current ip address intially read to first loop. Yet another loop and then a new IP at the last.
@@ -136,33 +139,35 @@ class TestMainLogic(unittest.TestCase):
         args.domain = 'example.com'
         args.interval = 2
         args.test = True
+        args.log = True
+        args.logdir = os.getcwd()
         mock_parse_args.return_value = args
 
         # Mock NameCom class and its methods
-        mock_name_com_instance = mock_name_com.return_value
-        mock_name_com_instance.read_host_record.return_value = None
+        mock_namecom_instance = mock_namecom.return_value
+        mock_namecom_instance.read_host_record.return_value = None
         
-        mock_name_com_instance.create_record.return_value = 12345
-        mock_name_com_instance.update_record.return_value = 12345
+        mock_namecom_instance.create_record.return_value = 12345
+        mock_namecom_instance.update_record.return_value = 12345
 
         try:
-            name_dot_com_update.main()
+            namecom_update.main()
         except SystemExit as e:
             self.assertEqual(e.code, 0)  # assert that the exit code is 0
     
         calls = mock_get.call_args_list
-        api_username_calls = [call for call in calls if call[0][0] == 'API_USERNAME']
-        api_token_calls = [call for call in calls if call[0][0] == 'API_TOKEN']
+        api_username_calls = [call for call in calls if call[0][0] == 'NAMECOM_APIUSERNAME']
+        api_token_calls = [call for call in calls if call[0][0] == 'NAMECOM_APITOKEN']
         self.assertEqual(len(api_username_calls), 1)
         self.assertEqual(len(api_token_calls), 1)
 
         # Assert that the methods were called with the correct arguments
-        mock_name_com.assert_called_with('api_username', 'api_token', 'example.com', 'host1')
-        mock_name_com_instance.read_host_record.assert_called_once()
-        mock_name_com_instance.create_record.assert_called_once_with('1.2.3.5')
+        mock_namecom.assert_called_with('api_username', 'api_token', 'example.com', 'host1')
+        mock_namecom_instance.read_host_record.assert_called_once()
+        mock_namecom_instance.create_record.assert_called_once_with('1.2.3.5')
         # Check that update_record is called with the expected arguments
         update_calls = [call(12345, '1.2.3.4'), call(12345, '1.2.3.5')]
-        mock_name_com_instance.update_record.assert_has_calls(update_calls)
+        mock_namecom_instance.update_record.assert_has_calls(update_calls)
 
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='name_com_dns',
+    name='namecom_dns',
     version='0.1',
     description='A script to run automatic dynamic DNS update of name.com DNS service, using their API.',
+    long_description='''\
+    This script allows you to update DNS records with the external IP address using the Name.com API.
+    It periodically checks the external IP address and updates the DNS records accordingly.
+    For detailed instructions and usage, please refer to the GitHub repository.
+    ''',
     author="Anders Fluur",
     author_email="your.email@example.com",
-    url="https://github.com/AnderFluur/name_com_dns",
+    url="https://github.com/AnderFluur/namecom_dns",
     packages=find_packages(),
-    data_files=[('name_com_dns', ['name_com_dns/name_com_dns.service'])],
+    data_files=[('namecom_dns', ['namecom_dns.service'])],
     install_requires=[
         'requests',
         'argparse',
@@ -16,7 +21,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'name_com_dns=name_com_dns.name_dot_com_update:main',
+            'namecom_dns=namecom_dns.namecom_update:main',
         ],
     },
 )
